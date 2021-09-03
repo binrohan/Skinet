30c12988efdbb8d647fb5ebac9f3b3907bd3c171
Bin Rohan <binrohan.cs@gmail.com>
2021-09-03 13:47:46 +0600

    Api architecture (3)

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,29 +1,52 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
-{
+namespace API.Controllers {
     [ApiController]
     [Route ("api/[controller]")]
-    public class ProductsController : ControllerBase {
-        private readonly StoreContext _context;
-        public ProductsController (StoreContext context) {
-            _context = context;
+    public class ProductsController : ControllerBase 
+    {
+        private readonly IProductRepository _repo;
+        public ProductsController (IProductRepository repo) 
+        {
+            _repo = repo; 
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts () {
-            var products = await _context.Products.ToListAsync();
+        public async Task<ActionResult<List<Product>>> GetProducts () 
+        {
+            var products = await _repo.GetProductsAsync();
+
             return Ok(products);
         }
 
         [HttpGet ("{id}")]
-        public async Task<ActionResult<Product>> GetProduct (int id) {
-            return await _context.Products.FindAsync(id);
+        public async Task<ActionResult<Product>> GetProduct (int id) 
+        {
+            var product = await _repo.GetProductByIdAsync(id);
+
+            return Ok(product);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            var productBrands = await _repo.GetProductBrandsAsync();
+
+            return Ok(productBrands);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductType()
+        {
+            var productTypes = await _repo.GetProductsTypeAsync();
+
+            return Ok(productTypes);
         }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -22,8 +23,9 @@ namespace API {
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services) {
-
+        public void ConfigureServices (IServiceCollection services) 
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers ();
             services.AddDbContext<StoreContext> (x => 
                 x.UseSqlite (_config.GetConnectionString("DefaultConnection")));
@@ -36,7 +38,8 @@ namespace API {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) 
+        {
             if (env.IsDevelopment ()) {
                 app.UseDeveloperExceptionPage ();
                 app.UseSwagger ();
