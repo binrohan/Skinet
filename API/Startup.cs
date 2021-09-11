1d6ab6378b827a9267a8f05fd40dffd8d4e1f49a
Bin Rohan <binrohan.cs@gmail.com>
2021-09-11 23:42:42 +0600

    Error handling and Swagger (5)

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Errors;
+using API.Extensions;
 using API.Helpers;
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -26,28 +29,30 @@ namespace API {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) 
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+            
             services.AddAutoMapper(typeof(MappingProfiles));
+
             services.AddControllers ();
+
             services.AddDbContext<StoreContext> (x => 
                 x.UseSqlite (_config.GetConnectionString("DefaultConnection")));
+            
+            services.AddApplicationService();
 
-            // No need for swagger, may exclude later
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddSwaggerDocumnetation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure (IApplicationBuilder app, IWebHostEnvironment env) 
         {
-            if (env.IsDevelopment ()) {
-                app.UseDeveloperExceptionPage ();
-                app.UseSwagger ();
-                app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "API v1"));
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            if (env.IsDevelopment ()) 
+            {
+                app.UseSwaggerDocumentation();
             }
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection ();
 
