33a465f6b93adf155cfd975e53463af480b858ae
Bin Rohan <binrohan.cs@gmail.com>
2021-09-15 00:10:33 +0600

    component http interface (8)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/IPagination';
+import { IProduct } from './models/IProduct';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: IProduct[] = [];
+  
+  constructor(private http: HttpClient) {
+    
+  }
+
+  ngOnInit(): void {
+    this.http.get<IPagination>('https://localhost:5001/api/products?pageSize=50')
+      .subscribe((res: IPagination) => {
+      this.products = res.data;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
